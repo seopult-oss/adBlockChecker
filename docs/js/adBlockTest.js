e7277c16f477dcf6a88981f7c4cda30d0e3dd36e
iagafonov <iagafonov@seopult.ru>
2017-11-22 18:15:14 +0300

    fix gulp

--- docs/js/adBlockTest.js
+++ docs/js/adBlockTest.js
@@ -1,6 +1,6 @@
 (function () {
     'use strict';
-    angular.module('testApp',['adBlockModule'])
+    angular.module('testApp', ['adBlockModule'])
         .component('testComponent', {
             template: '<h2>Test adBlock</h2><p>adblock status: {{$ctrl.isAdBlockEnabled}}</p><button ng-click="$ctrl.checkAdBlock()">test</button>',
             controller: ['adBlockChecker', testController]
